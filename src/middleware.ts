1e61d54c9322d69fa2c48d1e48eceeb48c046088
Yannick Lansink <yannick.lansink@live.nl>
2023-10-10 09:49:35 +0200

    added author to post

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,25 +11,6 @@ export default authMiddleware({
   },
 });
 
-// export default withClerkMiddleware(() => {
-//   console.log("Middleware running");
-//   return NextResponse.next();
-// });
-
-// export default authMiddleware({
-//     afterAuth(auth, req, evt) {
-//       // handle users who aren't authenticated
-//       if (!auth.userId && !auth.isPublicRoute) {
-//         return redirectToSignIn({ returnBackUrl: req.url });
-//       }
-//       // redirect them to organization selection page
-//       if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/org-selection"){
-//         const orgSelection = new URL('/org-selection', req.url)
-//         return NextResponse.redirect(orgSelection)
-//       }
-//     }
-//   });
-
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
