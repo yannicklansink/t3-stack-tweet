0448e2305084ae5ac91fdcadde8656ec0463ed56
Yannick Lansink <yannick.lansink@live.nl>
2023-10-10 08:26:33 +0200

    css change

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,12 @@ import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
+// This file essentially hooks up tRPC to your Next.js project via an API route,
+// allowing you to call your tRPC routes client-side seamlessly and typesafely.
+
+// Think of it like a bridge connecting two islands. One island is your Next.js frontend,
+// and the other is your tRPC backend logic. This bridge (the API handler) allows easy and safe passage between the two.
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
@@ -12,7 +18,7 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
           );
         }
       : undefined,
